cbaa5edd94c4e0de5149228f2866b092876cc027
Andy Carrell <andycarrell@users.noreply.github.com>
2017-06-02 00:09:04 +1200

    Adds semi colons to working-directory

--- _.hyper_plugins/local/working-directory.js
+++ _.hyper_plugins/local/working-directory.js
@@ -1,13 +1,13 @@
-const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd })
+const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
 
 exports.middleware = store => next => action => {
   const {
     type,
     config: { workingDirectory }
-  } = action
+  } = action;
 
   if (type === 'CONFIG_LOAD' || type === 'CONFIG_RELOAD') {
-    push(workingDirectory || process.env.HOME)
+    push(workingDirectory || process.env.HOME);
   }
 
   next(action);
