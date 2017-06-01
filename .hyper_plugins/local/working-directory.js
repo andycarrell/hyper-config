22a52cc6c3352903f4ccde27f86bc1a69580de95
Andy Carrell <andycarrell@users.noreply.github.com>
2017-06-02 10:57:36 +1200

    Incorrect deconstruction means this didn't work

--- .hyper_plugins/local/working-directory.js
+++ .hyper_plugins/local/working-directory.js
@@ -1,13 +1,10 @@
 const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
 
 exports.middleware = store => next => action => {
-  const {
-    type,
-    config: { workingDirectory }
-  } = action;
+  const { type, config } = action;
 
   if (type === 'CONFIG_LOAD' || type === 'CONFIG_RELOAD') {
-    push(workingDirectory || process.env.HOME);
+    push(config.workingDirectory || process.env.HOME);
   }
 
   next(action);
